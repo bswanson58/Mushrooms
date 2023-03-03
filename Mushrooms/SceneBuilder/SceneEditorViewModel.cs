204d921f0f18d05df12d784533c43294fbf433fc
Bill Swanson <bswanson58@gmail.com>
2023-03-02 19:33:53 -0600

    Included textual display of the transition/display times in the scene builder.

--- Mushrooms/SceneBuilder/SceneEditorViewModel.cs
+++ Mushrooms/SceneBuilder/SceneEditorViewModel.cs
@@ -46,6 +46,11 @@ namespace Mushrooms.SceneBuilder {
         public  RangeCollection<PaletteViewModel>   Palettes { get; }
         public  RangeCollection<LightingItem>       LightingList { get; }
 
+        public  string                              DisplayDuration => mDisplayDuration.ToString();
+        public  string                              DisplayJitter => mDisplayJitter.ToString();
+        public  string                              TransitionDuration => mTransitionDuration.ToString();
+        public  string                              TransitionJitter => mTransitionJitter.ToString();
+
         public  DelegateCommand                     CreateScene { get; }
 
         public SceneEditorViewModel( IHubManager hubManager, IPaletteProvider paletteProvider, ISceneProvider sceneProvider ) {
@@ -74,22 +79,38 @@ namespace Mushrooms.SceneBuilder {
 
         public int TransitionDurationSeconds {
             get => (int)mTransitionDuration.TotalSeconds;
-            set => mTransitionDuration = TimeSpan.FromSeconds( value );
+            set { 
+                mTransitionDuration = TimeSpan.FromSeconds( value );
+
+                RaisePropertyChanged( () => TransitionDuration );
+            }
         }
 
         public int TransitionDurationJitterSeconds {
             get => (int)mTransitionJitter.TotalSeconds;
-            set => mTransitionJitter = TimeSpan.FromSeconds( value );
+            set { 
+                mTransitionJitter = TimeSpan.FromSeconds( value );
+
+                RaisePropertyChanged( () => TransitionJitter );
+            }
         }
 
         public int DisplayDurationSeconds {
             get => (int)mDisplayDuration.TotalSeconds;
-            set => mDisplayDuration = TimeSpan.FromSeconds( value );
+            set { 
+                mDisplayDuration = TimeSpan.FromSeconds( value );
+
+                RaisePropertyChanged( () => DisplayDuration );
+            }
         }
 
         public int DisplayDurationJitterSeconds {
             get => (int)mDisplayJitter.TotalSeconds;
-            set => mDisplayJitter = TimeSpan.FromSeconds( value );
+            set { 
+                mDisplayJitter = TimeSpan.FromSeconds( value );
+
+                RaisePropertyChanged( () => DisplayJitter );
+            }
         }
 
         public string SceneName {
