70b00b9394ba18134b8cdbe0dc55ed2471b5f1e5
Bill Swanson <bswanson58@gmail.com>
2023-03-08 19:40:24 -0600

    Persisted the main window placement on the screen.

--- Mushrooms/MainWindow.xaml.cs
+++ Mushrooms/MainWindow.xaml.cs
@@ -1,10 +1,28 @@
-﻿namespace Mushrooms {
+﻿using System.ComponentModel;
+using System;
+using Mushrooms.Platform;
+using Mushrooms.Resources;
+
+namespace Mushrooms {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
         public MainWindow() {
             InitializeComponent();
+
+            Closing += OnWindowClosing;
+        }
+
+        private void OnWindowClosing( object ? sender, CancelEventArgs args ) {
+            Settings.Default.WindowPlacement = this.GetPlacement();
+            Settings.Default.Save();
+        }
+
+        protected override void OnSourceInitialized( EventArgs e ) {
+            base.OnSourceInitialized( e );
+
+            this.SetPlacement( Settings.Default.WindowPlacement );
         }
     }
 }
