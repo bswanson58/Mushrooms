751d8f15c2fc8ce524d3400f30cc875914062ef2
Bill Swanson <bswanson58@gmail.com>
2023-03-15 12:35:38 -0500

    Added a message dialog to display simple messages.

---
+++ Mushrooms/Dialogs/MessageDialogViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ReusableBits.Wpf.DialogService;
+using System;
+
+namespace Mushrooms.Dialogs {
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class MessageDialogViewModel : DialogAwareBase {
+        public  const string                    cTitle = "title";
+        public  const string                    cMessage = "message";
+
+        public  string                          Message { get; private set; }
+
+        public MessageDialogViewModel() {
+            Title = String.Empty;
+            Message = String.Empty;
+        }
+
+        public override void OnDialogOpened( IDialogParameters parameters ) {
+            Title = parameters.GetValue<string>( cTitle ) ?? String.Empty;
+            Message = parameters.GetValue<string>( cMessage ) ?? String.Empty;
+
+            RaiseAllPropertiesChanged();
+        }
+    }
+}
