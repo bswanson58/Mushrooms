784f4c8fb88a7077277e8810be22366a6c195543
Bill Swanson <bswanson58@gmail.com>
2023-03-16 15:08:03 -0500

    Selected a template for the swatches display of a garden scene based upon it being active.

---
+++ Mushrooms/Garden/ColorTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Mushrooms.Models;
+using ReusableBits.Wpf.Utility;
+
+namespace Mushrooms.Garden {
+    internal class ColorTemplateSelector : DataTemplateSelector {
+        public  DataTemplate ?  InactiveTemplate { get; set; }
+        public  DataTemplate ?  ActiveTemplate { get; set; }
+
+        public override DataTemplate ? SelectTemplate( object item, DependencyObject container ) {
+            var userControl = container.FindParent<ItemsControl>();
+
+            if( userControl is { DataContext: GardenSceneViewModel gardenScene }) {
+                return gardenScene.IsSceneOn ? 
+                    ActiveTemplate : 
+                    InactiveTemplate;
+            }
+
+            return InactiveTemplate;
+        }
+    }
+}
