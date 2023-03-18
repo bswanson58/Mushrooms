49efe841376307c297cb1c35949a7db225916bda
Bill Swanson <bswanson58@gmail.com>
2023-03-18 14:38:09 -0500

    Created a thumb for vertical only selection.

---
+++ Mushrooms/Controls/VerticalSelectionThumb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Mushrooms.Controls {
+    internal class VerticalSelectionThumb : Thumb {
+        public  float   CanvasSize { get; set; }
+        public  float   ThumbSize { get; set; }
+
+        public VerticalSelectionThumb() {
+            DragDelta += OnDragDelta;
+        }
+
+        private void OnDragDelta( object sender, DragDeltaEventArgs e ) {
+            if( DataContext is Control control ) {
+                double top = Canvas.GetTop( control );
+                double height = Canvas.GetBottom( control ) - top;
+                double change = Math.Max( 0, Math.Min( CanvasSize - ThumbSize, top + e.VerticalChange ));
+
+                Canvas.SetTop( control, change );
+            }
+        }
+    }
+}
